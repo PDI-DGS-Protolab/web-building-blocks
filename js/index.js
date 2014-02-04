86b590a31b4a7efba7ebffaddf082bf6b056a29f
MJ <recogemoras@gmail.com>
2014-02-04 13:20:15 +0100

    first attempt at linking component's html to the iframe on the index. Non functional for now.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
 $().ready( function() {
-	$( '.brand-tab' ).click( function() {
-    $( this ).toggleClass( 'brand-clicked' );
-    $( '.brand-tab' ).not(this).removeClass( 'brand-clicked' );
-		filter( $( this ).attr( 'id' ) );
-  } );
-} );
 
-function filter( brand ) {
-	console.log( 'filtro por ' + brand );
+	$('.brand-tab').click(function() {
+    $(this).toggleClass('brand-clicked');
+    $('.brand-tab').not(this).removeClass('brand-clicked');
+		filter($(this).attr('id'));
+  });
+
+  $('.comp-tab').click(function() {
+    $('#iframe').src = componentRoute($(this).attr('id'));
+    console.log($('#iframe').src);
+  });
+
+});
+
+function filter(brand) {
+	console.log('filtro por ' + brand);
+}
+
+function componentRoute(component) {
+  return '../templates/' + component + '.html';
 }
