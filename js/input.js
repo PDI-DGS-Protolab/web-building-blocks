2d96138b12fe3ead03bbfe969aa952f33e651be6
MJ <recogemoras@gmail.com>
2014-02-06 13:01:29 +0100

    refactor of the default input element

--- js/input.js
+++ js/input.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
  $('#full-name').focus(function() {
-     if( !$("#error-container").is(':visible') ) {
-      $("#input-hint").show();
-      $("#input-hint").text("Enter your full name as it appears on your card");
-      $("#error-container").hide();
+     if( !$('#error-container').is(':visible') ) {
+      $('#input-hint').show();
+      $('#input-hint').text('Enter your full name as it appears on your card');
+      $('#error-container').hide();
     }
   }); 
 
@@ -12,41 +12,41 @@ $(document).ready(function() {
     var str = $('#full-name').val();
 
     if(!containsLetters(str)) {
-      $("#input-hint").hide();
-      $("#error-container").show();
-      $(this).addClass("negative-box");
-      $("#error-message").text("Illegal characters. You can input only letters.");
-      $("#error-message").addClass("negative-check");
+      $('#input-hint').hide();
+      $('#error-container').show();
+      $(this).addClass('negative-box');
+      $('#error-message').text('Illegal characters. You can input only letters.');
+      $('#error-message').addClass('negative-check');
       nameCheck=false;
     }
 
     else if((str=='')||(str==undefined)) {
-      $("#input-hint").show();
-      $("#error-container").hide();
-      $("#error-message").text("");
-      $(this).removeClass("negative-box");
+      $('#input-hint').show();
+      $('#error-container').hide();
+      $('#error-message').text('');
+      $(this).removeClass('negative-box');
       nameCheck=false;
     }
 
     else {
-      $(this).removeClass("negative-box");
-      $("#error-container").hide();
-      $("#input-hint").show();
-      $("#input-hint").text("Enter your full name as it appears on your card");
-      $("#error-message").removeClass("negative-check");
+      $(this).removeClass('negative-box');
+      $('#error-container').hide();
+      $('#input-hint').show();
+      $('#input-hint').text('Enter your full name as it appears on your card');
+      $('#error-message').removeClass('negative-check');
       nameCheck=true;
     }
   });
 
   $('#full-name').blur(function() {
     var str = $('#full-name').val();
-    $("#input-hint").text("");
+    $('#input-hint').text('');
     if(!containsLetters(str)) {
-      $("#input-hint").hide();
-      $("#error-container").show();
-      $(this).addClass("negative-box");
-      $("#error-message").text("Illegal characters. You can input only letters.");
-      $("#error-message").addClass("negative-check");
+      $('#input-hint').hide();
+      $('#error-container').show();
+      $(this).addClass('negative-box');
+      $('#error-message').text('Illegal characters. You can input only letters.');
+      $('#error-message').addClass('negative-check');
       nameCheck=false;
       $('#full-name').focus().val();
     }
@@ -54,18 +54,20 @@ $(document).ready(function() {
 
 });
 
-$(document).on('click','#plus-btt',function () {
-    $(this).parent().parent().append( '<div><br><input id="special-full-name" type="text"></input><button id="plus-btt" class="special-full-name-btt"><img src="../img/more.png"/></button></div>' );
-    $(this).addClass("special-full-name-btt-less");
-    $(this).removeClass("special-full-name-btt");
-    $(this).html('<img src="../img/less.png" />');
-    $(this).attr("id","less-btt");
-});
-
-$(document).on('click','#less-btt',function () {
-    $(this).parent().remove();
-});
-
 function containsLetters(name) {
   return /^[a-zA-Z\u00e1\u00e9\u00ed\u00f3\u00fa\u00c1\u00c9\u00cd\u00d3\u00da\u00f1\u00d1 ]*$/.test(name);
 }
+
+$(document).on('click','.plus-btt',function () {
+    console.log($(this).attr('id'));
+
+    var newDiv = '<div><br><input class="special-full-name" type="text"></input><button class="plus-btt button"><img src="../img/more.png"/></button></div>';
+    $('#input-container-plus-less').append(newDiv);
+    
+    $(this).removeClass('plus-btt').addClass('less-btt');
+    $(this).html('<img src="../img/less.png"/>');
+});
+
+$(document).on('click','.less-btt',function () {
+    $(this).parent().remove();
+});
