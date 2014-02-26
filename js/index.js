5d345638d956407eb50f0a503b8e57776e61b9ab
marccomino <marccomino@gmail.com>
2014-02-26 14:47:15 +0100

    Adding the javascript needed for the drag and drop utility

--- js/index.js
+++ js/index.js
@@ -11,6 +11,27 @@ $().ready( function() {
       $('#all').addClass('active-brand');
     }
   });
+
+  $(".component-img").draggable({ 
+      containment:"#right-container",
+      revert: true
+  });
+
+  $("#testing-container").droppable({
+      tolerance: 'fit',
+
+      drop: function( event, ui ) {
+        if (ui.draggable.hasClass("normal")) {
+          $(this).append('<div class="input-container"><input id="full-name" type="text"></input><div id="input-hint"></div><div id="error-container"><a id="error-message"></a></div></div>');
+        } else if (ui.draggable.hasClass("refresh")) {
+          $(this).append('<div class="input-container"><input disabled class="special-full-name" type="text"></input><button class="refresh-btt btt-input"><img src="img/refresh.png"/></button></div>');
+        } else if (ui.draggable.hasClass("add")) {
+          $(this).append('<div class="input-container"><input class="special-full-name" type="text"></input><button class="plus-btt btt-input"><img src="img/more.png"/></button></div>');
+        } else if (ui.draggable.hasClass("quit")) {
+          $(this).append('<div class="input-container"><input class="special-full-name" type="text"></input><button class="less-btt btt-input"><img src="img/less.png"/></button></div>');
+      }
+    }
+  });
 });
 
 function hideComponentsVisualization(brand) {
