4f80a7ae72c07bd7ebf8d15eb31a810264c198a0
MJ RF <recogemoras@gmail.com>
2014-04-23 11:46:38 +0200

    PAYMENT FORM: Further refactor over date inputs JS, not fully functional yet. WIP

--- js/paymentform/ccdateinputs.js
+++ js/paymentform/ccdateinputs.js
@@ -63,40 +63,28 @@ function checkInputMonth () {
   var inputMonth = $('#card-expiration-month').val().toString();
   var inputYear = $('#card-expiration-year').val().toString(); 
 
-  if (inputYear == '' || inputYear == undefined || isMadeOfWhiteSpaces(inputYear)) {
-    if (inputMonth == ''  || inputMonth == undefined || isMadeOfWhiteSpaces(inputMonth)) {
-      $('#card-expiration-month').removeClass('negative-box');
-      $('#card-expiration-error-container').text("");
-      $('#card-expiration-error-container').hide();
+  if (isVoid(inputYear)) {
+    if (isVoid(inputMonth)) {
+      resetErrorContainer('month');
       $('#card-expiration-container').removeClass('correct-input');
     }
     else if (onlyContainsDigits(inputMonth)) {
-      if (inputMonth > 12 || inputMonth < 1) {
-        $('#card-expiration-month').addClass('negative-box');
-        $('#card-expiration-month').focus().val();
-        $('#card-expiration-error-container').show();
-        $('#card-expiration-error-container').text("Invalid Month entered.");
-        $('#card-expiration-error-container').addClass('negative-check');
-        $('#card-expiration-container').removeClass('correct-input');
+      if (!isValidMonth(inputMonth)) {
+        invalidMonthState();
       }
       else {
-        $('#card-expiration-error-container').text("");
-        $('#card-expiration-error-container').hide();
+        resetErrorContainer();
         $('#card-expiration-month').removeClass('negative-box');
       }
     }
     else {
-      $('#card-expiration-month').addClass('negative-box');
-      $('#card-expiration-month').focus().val();
-      $('#card-expiration-error-container').show();
-      $('#card-expiration-error-container').text("You can input only digits.");
-      $('#card-expiration-error-container').addClass('negative-check');
-      $('#card-expiration-container').removeClass('correct-input');
+      notOnlyDigitsState('month');
     }
   }
   else {
     checkExpiration('month');
   }
+  form.globalCheck();
 }
 
 function checkInputYear () {
@@ -105,170 +93,67 @@ function checkInputYear () {
   var inputMonth = $('#card-expiration-month').val().toString();
   var inputYear = $('#card-expiration-year').val().toString(); 
 
-  if (inputMonth == ''  || inputMonth == undefined || isMadeOfWhiteSpaces(inputMonth)) {
-    if (inputYear == '' || inputYear == undefined || isMadeOfWhiteSpaces(inputYear)) {
-      $('#card-expiration-year').removeClass('negative-box');
-      $('#card-expiration-error-container').text("");
-      $('#card-expiration-error-container').hide();
+  if (isVoid(inputMonth)) {
+    if (isVoid(inputYear)) {
+      resetErrorContainer('year');
       $('#card-expiration-container').removeClass('correct-input');
     }
     else if (onlyContainsDigits(inputYear)) {
-      if (inputYear.length == 2 && inputYear < currentYear) {
-        $('#card-expiration-year').addClass('negative-box');
-        $('#card-expiration-year').focus().val();
-        $('#card-expiration-error-container').show();
-        $('#card-expiration-error-container').text("Your card has expired or you have chosen a wrong expiration date.");
-        $('#card-expiration-error-container').addClass('negative-check');
-        $('#card-expiration-container').removeClass('correct-input');
+      if (inputYear.length == 2 && !isValidYear(inputYear, currentYear)) {
+        expiredCardState();
       }
       else {
-        $('#card-expiration-error-container').text("");
-        $('#card-expiration-error-container').hide();
+        resetErrorContainer();
         $('#card-expiration-year').removeClass('negative-box');
       }
     }
     else {
-      $('#card-expiration-year').addClass('negative-box');
-      $('#card-expiration-year').focus().val();
-      $('#card-expiration-error-container').show();
-      $('#card-expiration-error-container').text("You can input only digits.");
-      $('#card-expiration-error-container').addClass('negative-check');
-      $('#card-expiration-container').removeClass('correct-input');
+      notOnlyDigitsState('year');
     }
   }
   else {
     checkExpiration('year');
   }
+  form.globalCheck();
 }
 
 function checkExpiration (modifiedElmnt) {
   var currentDate = new Date();
   var currentYear = currentDate.getFullYear().toString().substr(2, 4);
-  var currentMonth = currentDate.getMonth();
+  var currentMonth = currentDate.getMonth() + 1;
   var inputMonth = $('#card-expiration-month').val().toString();
   var inputYear = $('#card-expiration-year').val().toString();
 
   if (onlyContainsDigits(inputMonth) && onlyContainsDigits(inputYear)) {
-    
-  }
-  else {
-    $('#card-expiration-' + modifiedElmnt).addClass('negative-box');
-    $('#card-expiration-' + modifiedElmnt).focus().val();
-    $('#card-expiration-error-container').show();
-    $('#card-expiration-error-container').text("You can input only digits.");
-    $('#card-expiration-error-container').addClass('negative-check');
-    $('#card-expiration-container').removeClass('correct-input');
-  }
-}
-
-function dateCheck() {
-  var currentDate = new Date();
-  var currentYear = currentDate.getFullYear().toString().substr(2, 4);
-  var currentMonth = currentDate.getMonth();
-  var inputMonth = $('#card-expiration-month').val();
-  var inputYear = $('#card-expiration-year').val(); 
-
-
-  if (!onlyContainsDigits(inputYear)) {
-    $('#card-expiration-error-container').show();
-    $('#card-expiration-year').addClass('negative-box');
-    $('#card-expiration-month').addClass('negative-box');
-    $('#card-expiration-error-container').text("You can input only digits.");
-    $('#card-expiration-error-container').addClass('negative-check');
-    $('#card-expiration-year').focus().val();
-    $('#card-expiration-container').removeClass('correct-input');
-  }
-  else if (!onlyContainsDigits(inputMonth)) {
-    $('#card-expiration-error-container').show();
-    $('#card-expiration-year').addClass('negative-box');
-    $('#card-expiration-month').addClass('negative-box');
-    $('#card-expiration-error-container').text("You can input only digits.");
-    $('#card-expiration-error-container').addClass('negative-check');
-    $('#card-expiration-month').focus().val();
-    $('#card-expiration-container').removeClass('correct-input');
-  }
-  else {
-    if (inputMonth > 12 || inputMonth < 1) {
-      $('#card-expiration-error-container').show();
-      $('#card-expiration-year').addClass('negative-box');
-      $('#card-expiration-month').addClass('negative-box');
-      $('#card-expiration-error-container').text("Invalid Month entered.");
-      $('#card-expiration-error-container').addClass('negative-check');
-      $('#card-expiration-year').focus().val();
+    if (isValidDate(inputMonth, currentMonth, inputYear, currentYear)) {
+      resetErrorContainer(['month', 'year']);
+      $('#card-expiration-container').addClass('correct-input');
+    }
+    else if (isVoid(inputMonth)) {
+      resetErrorContainer('month');
       $('#card-expiration-container').removeClass('correct-input');
     }
-    if (inputYear == currentYear) {
-      if (inputMonth < currentMonth) {
-        //ERROR
-        $('#card-expiration-error-container').show();
-        $('#card-expiration-year').addClass('negative-box');
-        $('#card-expiration-month').addClass('negative-box');
-        $('#card-expiration-error-container').text("Your card has expired or you have chosen a wrong expiration date.");
-        $('#card-expiration-error-container').addClass('negative-check');
-        $('#card-expiration-year').focus().val();
-        $('#card-expiration-container').removeClass('correct-input');
-      }
-      else if (inputMonth == '') {
-        //Must input a month
-        $('#card-expiration-month').attr('placeholder', currentMonth + 1);
-        $('#card-expiration-error-container').text("");
-        $('#card-expiration-error-container').hide();
-        $('#card-expiration-month').removeClass('negative-box');
-        $('#card-expiration-year').removeClass('negative-box');
-        $('#card-expiration-container').removeClass('correct-input');
-      }
-      else {
-        //EVERYTHING OK!
-        $('#card-expiration-error-container').hide();
-        $('#card-expiration-error-container').text("");
-        $('#card-expiration-year').removeClass('negative-box');
-        $('#card-expiration-month').removeClass('negative-box');
-        $('#card-expiration-container').addClass('correct-input');
-      }
-    }
-    else if (inputYear == '') {
-      //Must input a year
-      $('#card-expiration-year').attr('placeholder', currentYear);
-      $('#card-expiration-error-container').text("");
-      $('#card-expiration-error-container').hide();
-      $('#card-expiration-year').removeClass('negative-box');
-      $('#card-expiration-month').removeClass('negative-box');
+    else if (isVoid(inputYear)) {
+      resetErrorContainer('year');
       $('#card-expiration-container').removeClass('correct-input');
     }
-
-    else if (inputYear.length == 2 && inputYear < currentYear) {
-      //ERROR
-      $('#card-expiration-error-container').show();
-      $('#card-expiration-year').addClass('negative-box');
-      $('#card-expiration-month').addClass('negative-box');
-      $('#card-expiration-error-container').text("Your card has expired or you have chosen a wrong expiration date.");
-      $('#card-expiration-error-container').addClass('negative-check');
-      $('#card-expiration-year').focus().val();
-      $('#card-expiration-container').removeClass('correct-input');
+    else if (!isValidMonth(inputMonth)) {
+      invalidMonthState();
     }
-    else if (inputYear.length == 1) {
+    else if (inputYear.length == 2 && !isValidYear(inputYear, currentYear)) {
+      expiredCardState();
+    }
+    else if (inputYear.length < 2) {
+      resetErrorContainer('year');
       $('#card-expiration-container').removeClass('correct-input');
     }
     else {
-      if (inputMonth == '') {
-        //Must input a month
-        $('#card-expiration-month').attr('placeholder', currentMonth + 1);
-        $('#card-expiration-error-container').text("");
-        $('#card-expiration-error-container').hide();
-        $('#card-expiration-month').removeClass('negative-box');
-        $('#card-expiration-year').removeClass('negative-box');
-        $('#card-expiration-container').removeClass('correct-input');
-      }
-      else {
-        $('#card-expiration-error-container').text("");
-        $('#card-expiration-error-container').hide();
-        $('#card-expiration-year').removeClass('negative-box');
-        $('#card-expiration-month').removeClass('negative-box');
-        $('#card-expiration-container').addClass('correct-input');
-      }
+      expiredCardState();
     }
   }
-  form.globalCheck();
+  else {
+    notOnlyDigitsState(modifiedElmnt);
+  }
 }
 
 function dateCheckOnBlur() {
@@ -305,10 +190,69 @@ function dateCheckOnBlur() {
   form.globalCheck();
 }
 
-function onlyContainsDigits (numberToAnalyze) {
-  return /^\d*$/.test(numberToAnalyze);
+function onlyContainsDigits (number) {
+  return /^\d*$/.test(number);
 }
 
 function isMadeOfWhiteSpaces (name) {
   return name.match(/^\s*$/);
+}
+
+function isVoid (number) {
+  return number == undefined || isMadeOfWhiteSpaces(number);
+}
+
+function isValidMonth (inputMonth) {
+  return (inputMonth < 13 && inputMonth > 0);
+}
+
+function isValidYear (inputYear, currentYear) {
+  return inputYear >= currentYear;
+}
+
+function isValidDate (inputMonth, currentMonth, inputYear, currentYear) {
+  if (isValidMonth(inputMonth) && isValidYear(inputYear, currentYear) && inputYear.length == 2) {
+    if (inputYear > currentYear) {
+      return true;
+    }
+    else if (inputYear == currentYear && inputMonth >= currentMonth) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function invalidMonthState () {
+  $('#card-expiration-month').addClass('negative-box');
+  $('#card-expiration-month').focus().val();
+  $('#card-expiration-error-container').show();
+  $('#card-expiration-error-container').text("Invalid Month entered.");
+  $('#card-expiration-error-container').addClass('negative-check');
+  $('#card-expiration-container').removeClass('correct-input');
+}
+
+function notOnlyDigitsState (elmnt) {
+  $('#card-expiration-' + elmnt).addClass('negative-box');
+  $('#card-expiration-' + elmnt).focus().val();
+  $('#card-expiration-error-container').show();
+  $('#card-expiration-error-container').text("You can input only digits.");
+  $('#card-expiration-error-container').addClass('negative-check');
+  $('#card-expiration-container').removeClass('correct-input');
+}
+
+function expiredCardState () {
+  $('#card-expiration-year').addClass('negative-box');
+  $('#card-expiration-year').focus().val();
+  $('#card-expiration-error-container').show();
+  $('#card-expiration-error-container').text("Your card has expired or you have chosen a wrong expiration date.");
+  $('#card-expiration-error-container').addClass('negative-check');
+  $('#card-expiration-container').removeClass('correct-input');
+}
+
+function resetErrorContainer (elmnts) {
+  $('#card-expiration-error-container').hide();
+  $('#card-expiration-error-container').text("");
+  for (el in elmnts) {
+    $('#card-expiration-' + el).removeClass('negative-box');
+  }
 }
